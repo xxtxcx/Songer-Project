a2fa3757cc07099d002ce73974d319d88d3ca12f
andrii <andrcw@ukr.net>
2021-03-16 14:43:07 +0200

    first commit

---
+++ SongerAPI/Controllers/SongItemController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SongerAPI.Models;
+
+namespace SongerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongItemController : ControllerBase
+    {
+        private readonly SongItemContext _context;
+
+        public SongItemController(SongItemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SongItem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SongItem>>> GetSongItems()
+        {
+            return await _context.SongItems.ToListAsync();
+        }
+
+        // GET: api/SongItem/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SongItem>> GetSongItem(int id)
+        {
+            var songItem = await _context.SongItems.FindAsync(id);
+
+            if (songItem == null)
+            {
+                return NotFound();
+            }
+
+            return songItem;
+        }
+
+        // PUT: api/SongItem/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSongItem(int id, SongItem songItem)
+        {
+            if (id != songItem.SongId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(songItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SongItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SongItem
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SongItem>> PostSongItem(SongItem songItem)
+        {
+            _context.SongItems.Add(songItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSongItem", new { id = songItem.SongId }, songItem);
+        }
+
+        // DELETE: api/SongItem/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSongItem(int id)
+        {
+            var songItem = await _context.SongItems.FindAsync(id);
+            if (songItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.SongItems.Remove(songItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SongItemExists(int id)
+        {
+            return _context.SongItems.Any(e => e.SongId == id);
+        }
+    }
+}
---
+++ SongerAPI/Migrations/20210312182411_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SongerAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SongItems",
+                columns: table => new
+                {
+                    SongId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SongTitle = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    SongArtist = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    SongKey = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    SongBpm = table.Column<string>(type: "nvarchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SongItems", x => x.SongId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SongItems");
+        }
+    }
+}
